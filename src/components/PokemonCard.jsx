8b74382f29d4c78d059d80f7059cc8e5ac37be3e
Valeriane SUDRE CHEVALIER <valeriane.chevalier@gmail.com>
2023-10-10 11:55:50 +0200

    ajout components 1 PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,9 @@
+function PokemonCard() {
+    return <figures>
+    <img src=" https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+    <figcaption>
+        Bulbasaur
+    </figcaption>
+    </figures>;
+}
+export default PokemonCard
